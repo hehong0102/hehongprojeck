d874d8519f776ba8c32caf37991cfae8cfa9dea2
honghe <hehong0102@hotmail.com>
2020-10-18 10:25:56 +0200

    java

---
+++ invertion.java
@@ -0,0 +1,19 @@
+package testproject;
+
+public class invertion {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		System.out.println(a);
+		System.out.println(b);
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println(a);
+		System.out.println(b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
